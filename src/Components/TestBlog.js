eaad08562e84c5cd6523b1a25164a00da7611e1f
Jake <jacob.w.moreno@gmail.com>
2020-07-17 11:14:22 -0600

    Sass stopped working... First blog post is under way.

--- src/Components/TestBlog.js
+++ src/Components/TestBlog.js
@@ -9,15 +9,16 @@ const Blog = (props) => {
   return (<div className='obligatory-div'>
     <Header title='BLOG'/>
     <div className='mini-header' id='blue-header'>
-      <h1>THE PROS AND CONS OF WEB DEVELOPMENT BOOTCAMPS</h1>
+      <h1>MAKING SENSE OF JAVASCRIPT</h1>
       <span>December 31, 2020</span>
     </div>
     <div className = 'blog-content'>
       <div className='blog-section'>
-        <h1 className='header' id='orange'>Test Header</h1>
+        <h1 className='header' id='orange'>JavaScript Can Be Confusing</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut egestas nunc. Fusce ultricies nec diam sit amet imperdiet. Suspendisse tristique, dolor in eleifend ornare, felis sapien volutpat ante, sed imperdiet libero ligula vitae velit. Donec sed eros efficitur, tristique ipsum non, suscipit nisl. Donec id efficitur lectus. Cras pellentesque porta purus, nec pretium justo consectetur non. Sed condimentum leo nec urna ultrices porta. Nulla finibus accumsan libero.
+          
         </p>
+        <p style={{'marginTop':'15px', 'marginBottom':'15px'}}><strong>I simplify everything.</strong></p>
         <ul>
           <li>This is cool</li>
           <li>This is cool</li>
@@ -27,7 +28,21 @@ const Blog = (props) => {
         {/* <img className='img'></img> */}
       </div>
       <div className='blog-section'>
-        <h1 className='header' id='orange'>Test header 2</h1>
+        <h1 className='header' id='orange'>There Are Only 7 Data Types</h1>
+        <p>
+          And what's more, they can be categorized further:
+        </p>
+        <ul>
+          <li>Booleans</li>
+          <li>Null</li>
+          <li>Undefined</li>
+          <li>Numbers</li>
+          <li>BigInt</li>
+          <li>Strings</li>
+          <li>Arrays</li>
+          <li>Objects</li>
+        </ul>
+
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut egestas nunc. Fusce ultricies nec diam sit amet imperdiet. Suspendisse tristique, dolor in eleifend ornare, felis sapien volutpat ante, sed imperdiet libero ligula vitae velit. Donec sed eros efficitur, tristique ipsum non, suscipit nisl. Donec id efficitur lectus. Cras pellentesque porta purus, nec pretium justo consectetur non. Sed condimentum leo nec urna ultrices porta. Nulla finibus accumsan libero.
         </p>
