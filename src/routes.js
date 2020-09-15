d641ba734af19fa7b1a019f3a83a1a64964d58cc
Jake <jacob.w.moreno@gmail.com>
2020-09-14 23:21:25 -0600

    Got rid of unnecessary files (version 1)

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,6 @@ import {Switch, Route} from 'react-router-dom';
 
 // import WebDevelopment from './Components/WebDev';
 import SoftwareEngineer from './Components/V2/SoftwareEngineer/SoftwareEngineer'
-import SimplifyJS from './Components/SimplifyJS/SimplifyJS';
 import RacialApplications from './Components/V2/Blog/BlogPosts/RacialApplications';
 import SuckAtCoding from './Components/V2/Blog/BlogPosts/SuckAtCoding';
 import GetAJob from './Components/V2/Blog/BlogPosts/GetAJob';
@@ -12,7 +11,6 @@ import Blog from './Components/V2/Blog/Blog';
 export default(
   <Switch>
     <Route exact path='/' component={SoftwareEngineer}/>
-    <Route exact path='/blog/simplify-javascript' component = {SimplifyJS}/>
     <Route exact path='/blog/racial-applications' component = {RacialApplications}/>
     <Route exact path='/blog/how-to-suck-at-coding' component = {SuckAtCoding}/>
     <Route exact path='/blog/3-tips-to-land-your-next-coding-job' component = {GetAJob}/>
